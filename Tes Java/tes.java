512e27a4868cc9246c9c94346cf3fa86d30375f6
krisnabimantoro <krisnabmntr@gmail.com>
2023-02-22 17:12:28 +0700

    hapus comment

--- Tes Java/tes.java
+++ Tes Java/tes.java
@@ -1,18 +1,18 @@
 
 public class tes{
     
-    // definisi atribut
+   
     String name;
     int speed;
     int healthPoin;
 
-    // definisi method run
+   
     void run(){
         System.out.println(name +" is running...");
         System.out.println("Speed: "+ speed);
     }
 
-    // definisi method isDead untuk mengecek nilai kesehatan (healthPoin)
+
     boolean isDead(){
         if(healthPoin <= 0) return true;
         return false;
